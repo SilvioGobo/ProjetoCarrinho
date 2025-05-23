37d57d6b810f29de18dc4169f5843ce31b0262ff
Silvio Daniel Gomes Gobo <silviogomesbo@gmail.com>
2025-05-22 22:00:25 -0400

    rotas privadas

--- src/components/routers/index.jsx
+++ src/components/routers/index.jsx
@@ -3,23 +3,21 @@ import { Route, Routes } from 'react-router';
 import PaginaProdutos from '../../pages/Produtos/PaginaProdutos';
 import Login from '../../pages/Login/telaLogin';
 import Cadastro from '../../pages/Cadastro/telaCadastro';
+import PrivateRoute from './PrivateRoute';
 
 export default function Routers() {
   return (
-    <Routes>
+   <Routes>
       <Route
         path="/"
-        element={<PaginaProdutos/>}
+        element={
+          <PrivateRoute>
+            <PaginaProdutos />
+          </PrivateRoute>
+        }
       />
-      <Route
-        path="/login"
-        element={<Login/>}
-      />
-      <Route
-        path="/cadastro"
-        element={<Cadastro/>}
-      />
-
+      <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Cadastro />} />
     </Routes>
   );
 }
